b7c31ea7d126660c4abb527466f921df92ec1c39
sihanpopo <laphys@naver.com>
2026-06-05 10:14:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,21 @@
 import requests
 
-products = [
-    {
-        "site": "ssg",
-        "name": "SPB317",
-        "url": "https://www.ssgdfs.com/kr/goos/initDetailGoos?goos_cd=100618000150"
+url = "https://www.ssgdfs.com/kr/goos/initDetailGoos?goos_cd=100618000150"
+
+response = requests.get(
+    url,
+    headers={
+        "User-Agent": "Mozilla/5.0"
     },
-    {
-        "site": "ssg",
-        "name": "SWR107",
-        "url": "https://www.ssgdfs.com/kr/goos/initDetailGoos?goos_cd=100618000222"
-    },
-    {
-        "site": "shilla",
-        "name": "SPB317",
-        "url": "https://www.shilladfs.com/estore/kr/ko/p/5768726"
-    },
-    {
-        "site": "shilla",
-        "name": "SWR107",
-        "url": "https://www.shilladfs.com/estore/kr/ko/p/5746936"
-    }
-]
+    timeout=30
+)
 
-for product in products:
+html = response.text
 
-    html = requests.get(
-        product["url"],
-        headers={"User-Agent": "Mozilla/5.0"}
-    ).text
+print("HTTP STATUS:", response.status_code)
+print("HTML LENGTH:", len(html))
+print("notiReqBtn FOUND:", "notiReqBtn" in html)
 
-    if product["site"] == "ssg":
-        soldout = 'name="notiReqBtn"' in html
-
-    elif product["site"] == "shilla":
-        soldout = "requestRestockNotiBtn" in html
-
-    if soldout:
-        print(f"❌ 품절 - {product['name']}")
-    else:
-        print(f"✅ 재고 있음 - {product['name']}")
+print("\n===== HTML 시작 =====\n")
+print(html[:2000])
+print("\n===== HTML 끝 =====")
